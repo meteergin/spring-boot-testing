4b12c5ed58d19465bf3f526822181eab2b278814
Mete Ergin <mete.ergin@outlook.com>
2022-07-12 08:02:30 +0300

    First commit

---
+++ src/main/java/com/meteergin/testing/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.meteergin.testing.service;
+
+import com.meteergin.testing.model.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    Optional<Employee> getEmployeeById(long id);
+
+    Employee updateEmployee(Employee updatedEmployee);
+
+    void deleteEmployee(long id);
+}
